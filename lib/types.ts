412f8c978af5cbbf466d7a6841e4a8e53277d296
kirannambiar12 <kirannambiar12@gmail.com>
2026-04-01 11:57:30 +0530

    - Created utils to read, write, get serviceById, addService and delete serviceById to a json file which will be the our store - Created types - Created constants

---
+++ lib/types.ts
@@ -0,0 +1,28 @@
+export type ServiceStatus = "UP" | "SLOW" | "DOWN";
+
+export interface Service {
+  id: string;
+  name: string;
+  url: string;
+  status: ServiceStatus;
+  latencyMs: number | null;
+  lastCheckedAt: string | null;
+  healthScore: number;
+}
+
+export interface ApiError {
+  code: string;
+  message: string;
+}
+
+export interface ApiErrorResponse {
+  error: ApiError;
+}
+
+export interface ServicesListResponse {
+  services: Service[];
+}
+
+export interface ServiceResponse {
+  service: Service;
+}
